e518977718b0974f6b873618d5b272c9c8bd5fc2
sh-koh <70974710+sh-koh@users.noreply.github.com>
2024-09-18 14:31:30 +0200

    「🗑️」 clean(lib): removed unused functions

--- lib/default.nix
+++ lib/default.nix
@@ -1,25 +1,5 @@
-{
-  inputs,
-  config,
-  withSystem,
-  ...
-}:
+{ ... }:
 {
   flake.lib = {
-    mkNixos = sys: extraModules:
-      inputs.nixpkgs.lib.nixosSystem {
-        specialArgs = withSystem sys ({ inputs', self', ... }:
-          { inherit self' inputs' inputs; });
-        modules = [
-          inputs.nixpkgs.nixosModules.readOnlyPkgs
-          { nixpkgs.pkgs = withSystem sys ({ pkgs, ... }: pkgs); }
-        ] ++ extraModules;
-    };
-    mkHome = sys: modules:
-      withSystem sys ({ pkgs, self', inputs', ... }:
-        inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs modules;
-          extraSpecialArgs = {inherit self' inputs' inputs;};
-        });
   };
 }
