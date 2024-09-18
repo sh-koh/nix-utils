c5c05f7ddd33023d86ac4b89aab760582b1bda69
sh-koh <70974710+sh-koh@users.noreply.github.com>
2024-09-18 14:25:34 +0200

    「🔨」 fix(templates): remove hardcoded systems

--- templates/haskell/flake.nix
+++ templates/haskell/flake.nix
@@ -9,10 +9,7 @@
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
       perSystem =
         { pkgs, ... }:
         {
