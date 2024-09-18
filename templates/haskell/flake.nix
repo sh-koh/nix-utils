720ed0fd09a4644931897c7e7edd7f0b995fd01e
sh-koh <70974710+sh-koh@users.noreply.github.com>
2024-09-18 14:32:10 +0200

    「✨」 feat(templates): new haskell template using haskell-flake

--- templates/haskell/flake.nix
+++ templates/haskell/flake.nix
@@ -3,35 +3,43 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        { pkgs, ... }:
-        {
-          devShells.default = pkgs.mkShell { packages = with pkgs; [ ghc ]; };
-          packages.default = pkgs.stdenv.mkDerivation (
-            finalAttrs:
-            let
-              pname = "haskell-template";
-            in
-            {
-              inherit pname;
-              version = "git";
-              src = ./.;
-              buildInputs = with pkgs; [ ghc ];
-              nativeBuildInputs = with pkgs; [ ];
-              buildPhase = ''
-                mkdir -p $out/bin
-                ${pkgs.ghc}/bin/ghc src/Main.hs -o ${pname}
-                cp ${pname} $out/bin/${pname}
-              '';
-            }
-          );
-        };
-    };
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; }
+      {
+        imports = [
+          inputs.haskell-flake.flakeModule
+        ];
+
+        systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+        perSystem = { pkgs, self', ... }:
+          {
+            packages.default = self'.packages.template; # TODO: replace package name and '.cabal' file name
+            haskellProjects.default = {
+              basePackages = pkgs.haskellPackages;
+              packages = {
+                # Dependencies
+                # aeson.source = "1.5.0.0";
+              };
+              settings = {
+                # Settings for dependecies
+                # aeson = {
+                #   check = false;
+                # };
+              };
+              devShell = {
+                enable = true;
+                # tools = hp: { cabal = hp.cabal; };
+                hlsCheck.enable = false;
+              };
+            };
+          };
+      };
 }
